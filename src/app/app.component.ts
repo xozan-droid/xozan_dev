e1c6b38459d4f67bd7131ec37d307ccf2734f72a
Xozan Gonzalez <xozangonzalez@gmail.com>
2024-11-21 11:51:24 -0600

    subiendo mi proyecto a git para darle seguimiento si perder nada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DashboardComponent } from "./dashboard/dashboard.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
